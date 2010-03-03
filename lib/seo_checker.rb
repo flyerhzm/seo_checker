cabd98407bfef805d714d94d39cac1390d33ba97
Richard Huang <flyerhzm@gmail.com>
2010-03-03 22:17:59 +0800

    better report

--- lib/seo_checker.rb
+++ lib/seo_checker.rb
@@ -11,9 +11,14 @@ class SEOChecker
     @locations = []
     @titles = {}
     @descriptions = {}
-    @errors = []
-    @batch_size = options[:batch_size].to_i
-    @interval_time = options[:interval_time].to_i || 0
+    @id_urls = {}
+    @excessive_keywords = []
+    @nesting_subdirectories = []
+    @no_titles = []
+    @no_descriptions = []
+    @unreachables = []
+    @batch_size = options[:batch_size] ? options[:batch_size].to_i : nil
+    @interval_time = options[:interval_time].to_i
   end
 
   def check
@@ -45,29 +50,20 @@ class SEOChecker
       batch_locations.each do |location|
         response = get_response(URI.parse(location))
         if response.is_a? Net::HTTPSuccess
-          check_title(response, location)
-          check_description(response, location)
+          if response.body =~ %r{<head>(.*?)</head>}m
+            check_title($1, location)
+            check_description($1, location)
+          else
+            @no_titles << location
+            @no_descriptions << location
+          end
           check_url(location)
         else
-          @errors << "The page is unreachable #{location}."
+          @unreachables << location
         end
       end
       sleep(@interval_time)
     end
-  end
-
-  def report
-    @titles.each do |title, locations|
-      if locations.size > 1
-        @errors << "#{locations.slice(0, 5).join(', ')} #{'and ...' if locations.size > 5} have the same title '#{title}'."
-      end
-    end
-    @descriptions.each do |description, locations|
-      if locations.size > 1
-        @errors << "#{locations.slice(0, 5).join(', ')} #{'and ...' if locations.size > 5} have the same description '#{description}'."
-      end
-    end
-    puts @errors.join("\n")
   end
 
   private
@@ -78,34 +74,65 @@ class SEOChecker
       response = http.request(request)
     end
 
-    def check_title(response, location)
-      if response.body =~ %r{<title>(.*?)</title>}
+    def check_title(header_string, location)
+      if header_string =~ %r{<title>(.*?)</title>}
         title = $1
+        (@titles[title] ||= []) << location
       else
-        @errors << "#{location} has no title."
+        @no_titles << location
       end
-      (@titles[title] ||= []) << location
     end
 
-    def check_description(response, location)
-      if response.body =~ %r{<meta\s+name=["']description["']\s+content=["'](.*?)["']\s*/>|<meta\s+content=["'](.*?)["']\s+name=["']description["']\s*/>}
+    def check_description(header_string, location)
+      if header_string =~ %r{<meta\s+name=["']description["']\s+content=["'](.*?)["']\s*/>|<meta\s+content=["'](.*?)["']\s+name=["']description["']\s*/>}
         description = $1 || $2
+        (@descriptions[description] ||= []) << location
       else
-        @errors << "#{location} has no description."
+        @no_descriptions << location
       end
-      (@descriptions[description] ||= []) << location
     end
 
     def check_url(location)
       items = location.split('/')
       if items.find { |item| item =~ /^\d+$/ } || items.last =~ /^\d+\.htm(l)?/
-        @errors << "#{location} should not just use ID number in URL."
+        @id_urls[location.gsub(%r{/\d+/}, 'id')] = location
       end
       if items.find { |item| item.split('-').size > 5 }
-        @errors << "#{location} use excessive keywords"
+        @excessive_keywords << location
       end
       if items.size > 8
-        @errors << "#{location} has deep nesting of subdirectories"
+        @nesting_subdirectories << location
+      end
+    end
+
+    def report
+      unless @unreachables.empty?
+        print "#{@unreachables.slice(0, 5).join(",\n")} #{'and ...' if @unreachables.size > 5} are unreachable.\n\n"
+      end
+      unless @no_titles.empty?
+        print "#{@no_titles.slice(0, 5).join(",\n")} #{'and ...' if @no_titles.size > 5} have no title.\n\n"
+      end
+      unless @no_descriptions.empty?
+        print "#{@no_descriptions.slice(0, 5).join(",\n")} #{'and ...' if @no_descriptions.size > 5} have no description.\n\n"
+      end
+      @titles.each do |title, locations|
+        if locations.size > 1
+          print "#{locations.slice(0, 5).join(",\n")} #{'and ...' if locations.size > 5} have the same title '#{title}'.\n\n"
+        end
+      end
+      @descriptions.each do |description, locations|
+        if locations.size > 1
+          print "#{locations.slice(0, 5).join(",\n")} #{'and ...' if locations.size > 5} have the same description '#{description}'.\n\n"
+        end
+      end
+      unless @id_urls.empty?
+        print "#{@id_urls.values.slice(0, 5).join(",\n")} #{'and ...' if @id_urls.values.size > 5} use ID number in URL.\n\n"
+      end
+      unless @excessive_keywords.empty?
+        print "#{@excessive_keywords.slice(0, 5).join(",\n")} #{'and ...' if @excessive_keywords.size > 5} use excessive keywords in URL.\n\n"
+      end
+      unless @nesting_subdirectories.empty?
+        print "#{@nesting_subdirectories.slice(0, 5).join(",\n")} #{'and ...' if @nesting_subdirectories.size > 5} have deep nesting of subdirectories in URL.\n\n"
       end
     end
 end
